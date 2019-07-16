ba7f5c9f9bf98b5901e580267ec1262d9077a7ee
Juan Diego Bernate <jdbernate1@hotmail.com>
2019-07-15 23:54:52 -0300

    inicial

---
+++ js/pixel-art.js
@@ -0,0 +1,89 @@
+var nombreColores = ['White', 'LightYellow',
+  'LemonChiffon', 'LightGoldenrodYellow', 'PapayaWhip', 'Moccasin', 'PeachPuff', 'PaleGoldenrod', 'Bisque', 'NavajoWhite', 'Wheat', 'BurlyWood', 'Tan',
+  'Khaki', 'Yellow', 'Gold', 'Orange', 'DarkOrange', 'OrangeRed', 'Tomato', 'Coral', 'DarkSalmon', 'LightSalmon', 'LightCoral', 'Salmon', 'PaleVioletRed',
+  'Pink', 'LightPink', 'HotPink', 'DeepPink', 'MediumVioletRed', 'Crimson', 'Red', 'FireBrick', 'DarkRed', 'Maroon',
+  'Brown', 'Sienna', 'SaddleBrown', 'IndianRed', 'RosyBrown',
+  'SandyBrown', 'Goldenrod', 'DarkGoldenrod', 'Peru',
+  'Chocolate', 'DarkKhaki', 'DarkSeaGreen', 'MediumAquaMarine',
+  'MediumSeaGreen', 'SeaGreen', 'ForestGreen', 'Green', 'DarkGreen', 'OliveDrab', 'Olive', 'DarkOliveGreen', 'YellowGreen', 'LawnGreen',
+  'Chartreuse', 'GreenYellow', 'Lime', 'SpringGreen', 'LimeGreen',
+  'LightGreen', 'PaleGreen', 'PaleTurquoise',
+  'AquaMarine', 'Cyan', 'Turquoise', 'MediumTurquoise', 'DarkTurquoise', 'DeepSkyBlue',
+  'LightSeaGreen', 'CadetBlue', 'DarkCyan', 'Teal', 'Steelblue', 'LightSteelBlue', 'Honeydew', 'LightCyan',
+  'PowderBlue', 'LightBlue', 'SkyBlue', 'LightSkyBlue',
+  'DodgerBlue', 'CornflowerBlue', 'RoyalBlue', 'SlateBlue',
+  'MediumSlateBlue', 'DarkSlateBlue', 'Indigo', 'Purple', 'DarkMagenta', 'Blue',
+  'MediumBlue', 'DarkBlue', 'Navy', 'Thistle',
+  'Plum', 'Violet', 'Orchid', 'DarkOrchid', 'Fuchsia', 'Magenta', 'MediumOrchid',
+  'BlueViolet', 'DarkViolet', 'DarkOrchid',
+  'MediumPurple', 'Lavender', 'Gainsboro', 'LightGray', 'Silver', 'DarkGray', 'Gray',
+  'DimGray', 'LightSlateGray', 'DarkSlateGray', 'Black'
+];
+
+
+var paleta = document.getElementById('paleta')
+
+var grilla_pixeles=document.getElementById('grilla-pixeles')
+
+var colorElegido;
+
+var indicadorColor = document.getElementById('indicador-de-color');
+
+function actualizarPaleta(lista) {
+  for (var i = 0; i < lista.length; i++) {
+    var el = document.createElement('div')
+    el.style.backgroundColor = lista[i];
+    el.className = 'color-paleta';
+    paleta.appendChild(el);
+
+  }
+}
+
+function crearGrilla(){
+  for (var i = 0; i < 1750; i++) {
+    var pix = document.createElement('div');
+    pix.className='pixels';
+    grilla_pixeles.appendChild(pix);
+    
+  }
+
+}
+
+function elegirColor(){
+  var color = document.getElementById('paleta')
+  color.addEventListener("click", tomarColor);
+
+  function tomarColor(e){
+    indicadorColor.style.backgroundColor = e.target.style.backgroundColor;
+    // colorElegido = e.target.style.backgroundColor;
+
+
+
+  }
+}
+
+
+
+// Variable para guardar el elemento 'color-personalizado'
+// Es decir, el que se elige con la rueda de color.
+var colorPersonalizado = document.getElementById('color-personalizado');
+
+colorPersonalizado.addEventListener('change', 
+  (function() {
+    // Se guarda el color de la rueda en colorActual
+    colorActual = colorPersonalizado.value;
+    // Completar para que cambie el indicador-de-color al colorActual
+
+
+  })
+);
+
+
+function iniciar(){
+  actualizarPaleta(nombreColores);
+  crearGrilla();
+}
+
+
+
+
